2d2392b4a8de9ada8ada7633d09eb0a17e9cb00c
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-07-24 12:52:48 +0100

    refac

--- extensions/v0/frontend/src/App.svelte
+++ extensions/v0/frontend/src/App.svelte
@@ -4,12 +4,24 @@
   import GrayscaleOverlay from "./lib/components/GrayscaleOverlay.svelte";
   import Survey from "./lib/components/Survey.svelte";
   import { API_BASE_URL } from "$lib/constants";
+  import Timer from "$lib/components/Timer.svelte";
+  import Tap from "$lib/components/Tap.svelte";
 
-  let user = null;
+  let user = true;
+
+  const INTERVENTIONS = ["timer", "gray", "tap"];
+
+  let tab = "";
 </script>
 
 {#if user}
-  <GrayscaleOverlay show={true} />
+  {#if tab === "gray"}
+    <GrayscaleOverlay show={true} />
+  {:else if tab === "timer"}
+    <Timer show={true} />
+  {:else if tab === "tap"}
+    <Tap show={true} />
+  {/if}
 {:else}
   <div
     class="modal tw-fixed tw-top-0 tw-right-0 tw-left-0 tw-bottom-0 tw-bg-black/60 tw-w-full tw-min-h-screen tw-h-screen tw-flex tw-justify-center tw-z-[9999] tw-overflow-hidden tw-overscroll-contain"
@@ -44,3 +56,37 @@
     </div>
   </div>
 {/if}
+
+<div
+  class="modal tw-fixed tw-top-0 tw-right-0 tw-left-0 tw-w-full tw-min-h-screen tw-h-screen tw-flex tw-justify-center tw-z-[9999] tw-overflow-hidden tw-overscroll-contain"
+>
+  <div
+    class=" tw-flex tw-items-center tw-gap-1 tw-absolute tw-bottom-0 tw-right-0 tw-p-4"
+  >
+    {#if tab !== ""}
+      <div
+        class=" tw-px-3 tw-py-1.5 tw-text-xs tw-bg-white tw-text-gray-800 tw-rounded-lg"
+      >
+        {tab}
+      </div>
+    {/if}
+    <button
+      class=" tw-px-3 tw-py-2 tw-rounded-lg tw-outline-none tw-border-none tw-bg-white hover:tw-bg-gray-100 tw-text-gray-800 tw-transition tw-cursor-pointer"
+      on:click={() => {
+        tab = "timer";
+      }}>Timer</button
+    >
+    <button
+      class=" tw-px-3 tw-py-2 tw-rounded-lg tw-outline-none tw-border-none tw-bg-white hover:tw-bg-gray-100 tw-text-gray-800 tw-transition tw-cursor-pointer"
+      on:click={() => {
+        tab = "gray";
+      }}>Grayscale</button
+    >
+    <button
+      class=" tw-px-3 tw-py-2 tw-rounded-lg tw-outline-none tw-border-none tw-bg-white hover:tw-bg-gray-100 tw-text-gray-800 tw-transition tw-cursor-pointer"
+      on:click={() => {
+        tab = "tap";
+      }}>Tap</button
+    >
+  </div>
+</div>
---
+++ extensions/v0/frontend/src/lib/components/Timer.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let show = false;
+
+  export let delta = 0.1;
+  let grayscale = 0;
+
+  let isScrolling = false;
+  let scrollTimeout = null;
+
+  onMount(() => {
+    const mouseDownHandler = (e) => {
+      console.log("mousedown", e);
+      grayscale = Math.min(1, grayscale + delta);
+    };
+
+    // listen to scroll
+    const scrollHandler = (e) => {
+      if (!isScrolling) {
+        isScrolling = true;
+      }
+
+      // Clear our timeout throughout the scroll so it keeps resetting
+      clearTimeout(scrollTimeout);
+
+      scrollTimeout = setTimeout(function () {
+        isScrolling = false;
+        grayscale = Math.min(1, grayscale + delta);
+        console.log("Grayscale", grayscale);
+      }, 50); // 50 ms of no wheel events
+    };
+
+    window.addEventListener("wheel", scrollHandler);
+    // window.addEventListener("mousedown", mouseDownHandler);
+
+    return () => {
+      window.removeEventListener("wheel", scrollHandler);
+      // window.removeEventListener("mousedown", mouseDownHandler);
+    };
+  });
+</script>
+
+{#if show}
+  <div
+    class="tw-fixed tw-top-0 tw-right-0 tw-left-0 tw-bottom-0 tw-w-full tw-min-h-screen tw-h-screen tw-flex tw-justify-center tw-z-[9999999999] tw-overflow-hidden tw-overscroll-contain tw-pointer-events-none"
+    style="backdrop-filter: grayscale({grayscale});"
+  ></div>
+{/if}

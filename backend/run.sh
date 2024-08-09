39c54e7a13dba37b713707a8bc86de982920fd6d
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-08-09 16:47:42 +0200

    Update run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
-docker rm -f intervention || true
-docker build -t intervention .
-docker run -d -p 7777:7777 --env-file .env --name intervention --restart always intervention
+docker rm -f interventions || true
+docker build -t interventions .
+docker run -d -p 7777:7777 --env-file .env --name interventions --restart always interventions
 docker image prune -f
